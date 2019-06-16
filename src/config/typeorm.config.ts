4e7b336afe583286e9270b573b404c38414cdb62
trujic1000 <aleksandar.trujic@live.com>
2019-06-16 12:16:55 -0500

    config added

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Task } from '../tasks/task.entity';
 import { User } from '../auth/user.entity';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123456',
-  database: 'taskmanagement',
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DBNAME || dbConfig.database,
   entities: [Task, User],
-  synchronize: true,
+  synchronize: process.env.TYPEORM_SNC || dbConfig.synchronize,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000;
+
+  const PORT = process.env.PORT || serverConfig.port;
   await app.listen(3000);
   logger.log(`Application listening on port ${PORT}`);
 }
